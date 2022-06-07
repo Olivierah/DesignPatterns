16a296d0114ebf6d27d9e11181cc03977b78570f
Henrique Oliveira <34348180+Olivierah@users.noreply.github.com>
2022-06-07 11:33:19 -0300

    update

--- 3-Template_Method/DesignPatterns/DesignPatterns/Program.cs
+++ 3-Template_Method/DesignPatterns/DesignPatterns/Program.cs
@@ -1,20 +1,7 @@
 ﻿// Projeto Parte 3 : 
 
-// É um padrão que  
+// É um padrão 
 
 using DesignPatterns;
 
-CalculadorDeDesconto calculadora = new CalculadorDeDesconto();
 
-Orcamento orcamento = new Orcamento(8000);
-
-orcamento.AdicionaItem(new Item("GELADEIRA", 1300));
-orcamento.AdicionaItem(new Item("PS5", 2500));
-orcamento.AdicionaItem(new Item("Xbox Série X", 1999));
-orcamento.AdicionaItem(new Item("TV", 1800));
-orcamento.AdicionaItem(new Item("FOGAO", 900));
-//orcamento.AdicionaItem(new Item("MICROONDAS", 700)); //=> Descomente para aplicar o Desconto para mais de 5 itens
-
-double desconto = calculadora.Calcula(orcamento);
-Console.WriteLine(desconto);
-Console.ReadKey();
